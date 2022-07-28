278b625c21c283c074342904268e8c57035e6c94
Aditi087 <aditijana0@gmail.com>
2022-07-28 17:18:37 +0530

    conflict

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 import * as React from "react";
 import Devices from "../devices/Devices";
 import Footer from "../footer/Footer";
-import Header from "../header/Header";
+import Header from "../home/Header/Header";
 import News from "../news/News";
 import Offer from "../offer/Offer";
 import User from "../userSay/User";
@@ -20,18 +19,6 @@ const Home = () => {
       <Vaccinatated />
       <News />
       <Footer />
-=======
-import Getfix from './Get-fix/Get-fix'
-import HeaderCarousel from './Header/HeaderCarousel'
-import Offer from './OfferCarousel/Offer'
-
-const Home: React.FunctionComponent = () => {
-  return (
-    <>
-      <HeaderCarousel />
-      <Getfix />
-      <Offer />
->>>>>>> 66a5ca7bfd3568e5ecc59b9a30b614cb91cb651d
     </>
   )
 }
--- src/Components/Router/Router.tsx
+++ src/Components/Router/Router.tsx
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
 import * as React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../home/Home';
-=======
-import * as React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Brand from '../Home/Get-fix/Brand/Brand'
-import Header from '../Home/Header/Header'
-import Home from '../Home/Home'
->>>>>>> 66a5ca7bfd3568e5ecc59b9a30b614cb91cb651d
+import Header from '../home/Header/Header';
 
 const Router: React.FunctionComponent = () => {
   return (
     <>
-      <Header />
+      <Header/>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/brand' element={<Brand />} />
       </Routes>
     </>
   )
