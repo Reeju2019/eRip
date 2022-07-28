87a33b3f3545f1f5fdb6db1c32deca03234ff47a
Mihir Baidya <mihirbaidya08@gmail.com>
2022-07-28 13:58:42 +0530

    add esLint roules, fix background image

---
+++ src/Components/Home/OfferCarousel/OfferCarouselCard.tsx
@@ -0,0 +1,13 @@
+import { Image } from 'react-bootstrap'
+
+const OfferCarouselCard: React.FunctionComponent = () => {
+  return (
+    <>
+      <div className='carousel-item'>
+        <Image src='static/Offer_carousel/Offer9.png' className='d-block h-100 w-auto' alt='...' />
+      </div>
+    </>
+  )
+}
+
+export default OfferCarouselCard
