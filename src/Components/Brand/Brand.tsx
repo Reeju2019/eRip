783499ac2fb2040c84a773d31e87f735e44167e5
Aditi087 <aditijana0@gmail.com>
2022-08-08 13:16:25 +0530

    error_fix

--- src/Components/Brand/Brand.tsx
+++ src/Components/Brand/Brand.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
 import Footer from '../footer/Footer'
 import Header from '../Home/Header/Header'
 import UserSay from '../Home/userSay/User'
--- src/Components/Issue/IssueHeader/IssueHeader.tsx
+++ src/Components/Issue/IssueHeader/IssueHeader.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Container } from 'react-bootstrap'
 import { Link, useParams } from 'react-router-dom'
 
