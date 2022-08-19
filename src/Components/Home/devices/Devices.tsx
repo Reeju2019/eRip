d198df13de917f9084389dfd81243aab12f5b0a2
Alok Mondal <alok.webideasolution@gmail.com>
2022-08-19 18:15:06 +0530

    slider update

--- src/Components/Home/devices/Devices.tsx
+++ src/Components/Home/devices/Devices.tsx
@@ -1,109 +1,130 @@
 import React from 'react'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+// import Slider from 'react-slick'
+// import 'slick-carousel/slick/slick.css'
+// import 'slick-carousel/slick/slick-theme.css'
 import './devices.css'
 import { Container } from 'react-bootstrap'
 import { Banner } from './Banner'
 import device from '../../../Data/Device.data.json'
 import data from '../../../Data/const.data.json'
 
-const Devices = () => {
-  const SlickArrowLeft = ({ currentSlide, ...props }: any) => (
-    <button
-      {...props}
-      className={'slick-prev slick-arrow' + (currentSlide === 0 ? ' slick-disabled' : '')}
-      aria-hidden='true'
-      aria-disabled={currentSlide === 0 ? true : false}
-      type='button'
-    >
-      Previous
-    </button>
-  )
-  const SlickArrowRight = ({ currentSlide, slideCount, ...props }: any) => (
-    <button
-      {...props}
-      className={
-        'slick-next slick-arrow' + (currentSlide === slideCount - 1 ? ' slick-disabled' : '')
-      }
-      aria-hidden='true'
-      aria-disabled={currentSlide === slideCount - 1 ? true : false}
-      type='button'
-    >
-      Next
-    </button>
-  )
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
 
-  const settings = {
-    accessibility: true,
-    adaptiveHeight: false,
-    arrows: true,
-    autoplay: false,
-    autoplaySpeed: 3000,
-    centerMode: false,
-    centerPadding: '50px',
-    cssEase: 'ease',
-    dots: false,
-    dotsClass: 'slick-dots',
-    draggable: true,
-    easing: 'linear',
-    edgeFriction: 0.15,
-    fade: false,
-    focusOnSelect: false,
-    focusOnChange: false,
-    infinite: false,
-    initialSlide: 0,
-    mobileFirst: false,
-    nextArrow: <SlickArrowRight />,
-    pauseOnDotsHover: false,
-    pauseOnFocus: true,
-    pauseOnHover: true,
-    prevArrow: <SlickArrowLeft />,
-    respondTo: 'window',
-    speed: 500,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-    rows: 1,
-    rtl: false,
-    slide: '',
-    slidesPerRow: 1,
-    slidesToScroll: 1,
-    slidesToShow: 4,
-    swipe: true,
-    swipeToSlide: false,
-    touchMove: true,
-    touchThreshold: 5,
-    useCSS: true,
-    useTransform: true,
-    variableWidth: false,
-    vertical: false,
-    verticalSwiping: false,
-    waitForAnimate: true,
-    zIndex: 1000,
+const Devices = () => {
+  // const SlickArrowLeft = ({ currentSlide, ...props }: any) => (
+  //   <button
+  //     {...props}
+  //     className={'slick-prev slick-arrow' + (currentSlide === 0 ? ' slick-disabled' : '')}
+  //     aria-hidden='true'
+  //     aria-disabled={currentSlide === 0 ? true : false}
+  //     type='button'
+  //   >
+  //     Previous
+  //   </button>
+  // )
+  // const SlickArrowRight = ({ currentSlide, slideCount, ...props }: any) => (
+  //   <button
+  //     {...props}
+  //     className={
+  //       'slick-next slick-arrow' + (currentSlide === slideCount - 1 ? ' slick-disabled' : '')
+  //     }
+  //     aria-hidden='true'
+  //     aria-disabled={currentSlide === slideCount - 1 ? true : false}
+  //     type='button'
+  //   >
+  //     Next
+  //   </button>
+  // )
+
+  // const settings = {
+  //   accessibility: true,
+  //   adaptiveHeight: false,
+  //   arrows: true,
+  //   autoplay: false,
+  //   autoplaySpeed: 3000,
+  //   centerMode: false,
+  //   centerPadding: '50px',
+  //   cssEase: 'ease',
+  //   dots: false,
+  //   dotsClass: 'slick-dots',
+  //   draggable: true,
+  //   easing: 'linear',
+  //   edgeFriction: 0.15,
+  //   fade: false,
+  //   focusOnSelect: false,
+  //   focusOnChange: false,
+  //   infinite: false,
+  //   initialSlide: 0,
+  //   mobileFirst: false,
+  //   nextArrow: <SlickArrowRight />,
+  //   pauseOnDotsHover: false,
+  //   pauseOnFocus: true,
+  //   pauseOnHover: true,
+  //   prevArrow: <SlickArrowLeft />,
+  //   respondTo: 'window',
+  //   speed: 500,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 3,
+  //         slidesToScroll: 3,
+  //         infinite: true,
+  //         dots: true,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 600,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 2,
+  //         initialSlide: 2,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 480,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //       },
+  //     },
+  //   ],
+  //   rows: 1,
+  //   rtl: false,
+  //   slide: '',
+  //   slidesPerRow: 1,
+  //   slidesToScroll: 1,
+  //   slidesToShow: 4,
+  //   swipe: true,
+  //   swipeToSlide: false,
+  //   touchMove: true,
+  //   touchThreshold: 5,
+  //   useCSS: true,
+  //   useTransform: true,
+  //   variableWidth: false,
+  //   vertical: false,
+  //   verticalSwiping: false,
+  //   waitForAnimate: true,
+  //   zIndex: 1000,
+  // }
+
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 2,
+      slidesToSlide: 1, // optional, default to 1.
+    },
   }
 
   return (
@@ -119,7 +140,24 @@ const Devices = () => {
             )
           })}
 
-          <Slider {...settings}>
+          {/* <Slider {...settings}> */}
+          <Carousel
+            swipeable={true}
+            draggable={true}
+            showDots={true}
+            responsive={responsive}
+            ssr={true} // means to render carousel on server-side.
+            infinite={true}
+            autoPlay={false}
+            autoPlaySpeed={1000}
+            keyBoardControl={true}
+            customTransition='all .5'
+            transitionDuration={500}
+            containerClass='carousel-container'
+            removeArrowOnDeviceType={['tablet', 'mobile']}
+            dotListClass='custom-dot-list-style'
+            itemClass='carousel-item-padding-40-px'
+          >
             {device.device.map((e, index) => {
               return (
                 <div className='card device_card' key={index}>
@@ -133,7 +171,9 @@ const Devices = () => {
                 </div>
               )
             })}
-          </Slider>
+          </Carousel>
+
+          {/* </Slider> */}
         </div>
       </Container>
     </div>
--- src/Components/Home/userSay/User.tsx
+++ src/Components/Home/userSay/User.tsx
@@ -1,108 +1,28 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
 import './user.css'
 import data from '../../../Data/const.data.json'
 import userData from '../../../Data/User.data.json'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
 
 const User = () => {
-  const SlickArrowLeft = ({ currentSlide, ...props }: any) => (
-    <button
-      {...props}
-      className={'slick-prev slick-arrow' + (currentSlide === 0 ? ' slick-disabled' : '')}
-      aria-hidden='true'
-      aria-disabled={currentSlide === 0 ? true : false}
-      type='button'
-    >
-      Previous
-    </button>
-  )
-  const SlickArrowRight = ({ currentSlide, slideCount, ...props }: any) => (
-    <button
-      {...props}
-      className={
-        'slick-next slick-arrow' + (currentSlide === slideCount - 1 ? ' slick-disabled' : '')
-      }
-      aria-hidden='true'
-      aria-disabled={currentSlide === slideCount - 1 ? true : false}
-      type='button'
-    >
-      Next
-    </button>
-  )
-
-  const settings = {
-    accessibility: true,
-    adaptiveHeight: false,
-    arrows: true,
-    autoplay: false,
-    autoplaySpeed: 3000,
-    centerMode: false,
-    centerPadding: '50px',
-    cssEase: 'ease',
-    dots: false,
-    dotsClass: 'slick-dots',
-    draggable: true,
-    easing: 'linear',
-    edgeFriction: 0.15,
-    fade: false,
-    focusOnSelect: false,
-    focusOnChange: false,
-    infinite: false,
-    initialSlide: 0,
-    mobileFirst: false,
-    nextArrow: <SlickArrowRight />,
-    pauseOnDotsHover: false,
-    pauseOnFocus: true,
-    pauseOnHover: true,
-    prevArrow: <SlickArrowLeft />,
-    respondTo: 'window',
-    speed: 500,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-    rows: 1,
-    rtl: false,
-    slide: '',
-    slidesPerRow: 1,
-    slidesToScroll: 1,
-    slidesToShow: 4,
-    swipe: true,
-    swipeToSlide: false,
-    touchMove: true,
-    touchThreshold: 5,
-    useCSS: true,
-    useTransform: true,
-    variableWidth: false,
-    vertical: false,
-    verticalSwiping: false,
-    waitForAnimate: true,
-    zIndex: 1000,
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+      slidesToSlide: 1, 
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 2,
+      slidesToSlide: 1, 
+    },
   }
 
   return (
@@ -112,7 +32,26 @@ const User = () => {
           return (
             <div key={index}>
               <h4 className='user_heading d-flex justify-content-center'>{c.heading}</h4>
-              <Slider {...settings}>
+              <Carousel
+                swipeable={true}
+                draggable={true}
+                showDots={true}
+                responsive={responsive}
+                ssr={true}
+                infinite={true}
+                autoPlay={false}
+                autoPlaySpeed={1000}
+                keyBoardControl={true}
+                customTransition='all .5'
+                transitionDuration={500}
+                containerClass='carousel-container'
+                removeArrowOnDeviceType={['tablet', 'mobile']}
+                dotListClass='custom-dot-list-style'
+                itemClass='carousel-item-padding-40-px'
+                partialVisible={true}
+                rewindWithAnimation={true}
+                sliderClass='react-multi-carousel-track'
+              >
                 {userData.user.map((e, index) => {
                   return (
                     <div key={index} className='card user_card'>
@@ -132,7 +71,7 @@ const User = () => {
                     </div>
                   )
                 })}
-              </Slider>
+              </Carousel>
             </div>
           )
         })}
